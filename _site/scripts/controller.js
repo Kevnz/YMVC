0d7be1a78b083f7861fe581495e9109979b32503
Kevin Isom <kevin.isom@gmail.com>
2011-05-06 18:46:56 +1200

    Added a spiked page as well

---
+++ _site/scripts/controller.js
@@ -0,0 +1,18 @@
+YUI.add('ymvc-model', function(Y){
+	
+	var Controller = Y.Base.create('Controller', Y.Base, [], {
+			initializer:function(){
+				Y.log('in the initializer','info', 'ymvc-model');
+			}
+		},{
+			ATTRS :{
+				title:{value:null},
+				id:{value:null}
+			}
+		}
+	);
+    Y.Controller = Controller;
+
+	
+			
+},  '0.0.1', {requires:['base', 'substitute']});
